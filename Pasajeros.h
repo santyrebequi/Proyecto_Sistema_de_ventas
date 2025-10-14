d3b1bb6cbce6db7e8f84bd0a1366947a70c0e07f
Oleos <leonardo.rua@outlook.es>
2025-10-14 14:54:20 -0300

    nada, solo cambios en Pasajeros.h

--- Pasajeros.h
+++ Pasajeros.h
@@ -1,14 +1,15 @@
 #ifndef PASAJEROS_H
 #define PASAJEROS_H
 
+// Definiciones de constantes para las variables de los pasajeros
 #define NOMBRE_MAX 50
 #define APELLIDO_MAX 50
 #define EMAIL_MAX 50
 #define TELEFONO_MAX 15
 #define DIR_MAX 100
-
+#define IDPASAJERO_MAX 5
 struct Pasajero {
-    int id[5];
+    char idpasajero[IDPASAJERO_MAX];
     char nombre[NOMBRE_MAX];
     char apellido[APELLIDO_MAX];
     char email[EMAIL_MAX];
