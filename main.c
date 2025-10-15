ef1818bd1cc0f8b4249dc838c3f252de3a24ec48
IanCurufil <curufil.ian@gmail.com>
2025-10-14 23:21:05 -0300

    ian

--- main.c
+++ main.c
@@ -4,7 +4,6 @@ int main(void) {
     printf("Hello, World!\n");
     printf("PROYECTO DE MIERDA\n");
     printf("Hola gente");
-    printf("Hola gente2.0");
-    printf("Aguante cuadrado");
+    
     return 0;
 }
