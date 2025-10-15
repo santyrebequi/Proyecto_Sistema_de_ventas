8b656338e94b9a4123950238b9dd42452c9caaca
IanCurufil <curufil.ian@gmail.com>
2025-10-15 16:40:21 -0300

    s

--- main/main.c
+++ main/main.c
@@ -4,6 +4,7 @@ int main(void) {
     printf("Hello, World!\n");
     printf("PROYECTO DE MIERDA\n");
     printf("Hola gente");
+    printf("Que onda son re giles no saben subir nada");
     
     return 0;
 }
