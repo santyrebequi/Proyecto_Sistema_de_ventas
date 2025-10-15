96ea22bbed643f9f009378284c044366cd39aad5
Oleos <leonardo.rua@outlook.es>
2025-10-15 14:30:40 -0300

    Estoy probando un error persistente de incluir los headers al .c

--- funciones/pasajes.c
+++ funciones/pasajes.c
@@ -1,10 +1,4 @@
-#ifndef PASAJES_H
-#define PASAJES_H
-
-#define ID_MAX 5
-#define BUTACA_MAX 2
-#define DESTINO_MAX 50
-#define Fecha_MAX 11
-#define HORARIO_MAX 5
-#define COSTO_MAX 10
-#define 
+#include <stdio.h>
+#include <stdlib.h>
+#include "Pasajeros.h"
+#include "pasajes.h"
