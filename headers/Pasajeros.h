2fd491fad810c44854808e34338b0e5bc25bf7bb
Oleos <leonardo.rua@outlook.es>
2025-10-16 17:07:43 -0300

    Estaba haciendo una funcion que no tiene sentido si no tengo la estructura de datos del pasajero porque no puedo registrar pasajes sin pasajeros debo de hacerlo antes del pasaje, no a la vez por lo que debo crear la estructura de datos y la funcion para registrar pasajeros

--- headers/Pasajeros.h
+++ headers/Pasajeros.h
@@ -4,6 +4,7 @@
 // Definiciones de constantes para las variables de los pasajeros
 #define NOMBRE_MAX 30
 #define APELLIDO_MAX 30
+#define DNI_MAX 15
 #define EMAIL_MAX 30
 #define TELEFONO_MAX 15
 #define DIR_MAX 100
@@ -13,9 +14,12 @@ struct Pasajero {
     char idpasajero[IDPASAJERO_MAX];
     char nombre[NOMBRE_MAX];
     char apellido[APELLIDO_MAX];
+    char dni[DNI_MAX];
     char email[EMAIL_MAX];
     char telefono[TELEFONO_MAX];
     char direccion[DIR_MAX];
 };
 
+void RegistrarPasajero(struct Pasajero *pasajeros, int *cantidadPasajeros);
+void ListarPasajeros(const struct Pasajero *pasajeros, int cantidadPasajeros);
 #endif // PASAJEROS_H
