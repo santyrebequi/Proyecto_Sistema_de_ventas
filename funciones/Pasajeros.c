16906ceb628a0a83c24d3e58a286c0cc1a64beef
Oleos <leonardo.rua@outlook.es>
2025-10-19 21:56:32 -0300

    Refactorizado la función de validación de DNI para mejorar su legibilidad y eficiencia.

--- funciones/Pasajeros.c
+++ funciones/Pasajeros.c
@@ -3,29 +3,37 @@
 // Usar rutas relativas al directorio headers para que el indexador de VS Code
 // (cpptools) y el compilador encuentren los archivos de cabecera. */
 #include "../headers/Pasajeros.h"
-#include "../headers/Horas.h"
 #include "../headers/Fecha.h"
+#include "../headers/pasajes.h"
 
-void RegistrarPasajero(struct Pasajero *pasajeros )
+void RegistrarPasajero(struct Pasajero *pasajeros, int *cantidadPasajeros)
 {
-    if (pasajeros == IDPASAJERO_MAX) {
+    if (*cantidadPasajeros >= IDPASAJERO_MAX)
+    {
         printf("No se pueden registrar más pasajeros. Límite alcanzado.\n");
         return;
     }
     struct Pasajero nuevoPasajero;
-
+    printf("---------------------------------------\n");
+    printf("----- Registro de Nuevo Pasajero -----\n");
     printf("Ingrese el nombre del pasajero: ");
     scanf("%s", nuevoPasajero.nombre);
     printf("Ingrese el apellido del pasajero: ");
     scanf("%s", nuevoPasajero.apellido);
     printf("Ingrese el DNI del pasajero: ");
     scanf("%s", nuevoPasajero.dni);
-    printf("Ingrese el email del pasajero: ");
+    printf("Ingrese email del pasajero: ");
     scanf("%s", nuevoPasajero.email);
     printf("Ingrese el teléfono del pasajero: ");
     scanf("%s", nuevoPasajero.telefono);
     printf("Ingrese la dirección del pasajero: ");
     scanf("%s", nuevoPasajero.direccion);
-    pasajeros[IDPASAJERO_MAX] = nuevoPasajero;
-    printf("Pasajero registrado con éxito.\n");
+    printf("---------------------------------------\n");
+
+    // Generar ID automático (ej: "00001", "00002", etc.)
+    snprintf(nuevoPasajero.idpasajero, IDPASAJERO_MAX, "%05d", *cantidadPasajeros + 1);
+    pasajeros[*cantidadPasajeros] = nuevoPasajero;
+    (*cantidadPasajeros)++;
+    printf("Pasajero registrado exitosamente. ID asignado: %s\n", nuevoPasajero.idpasajero);
+    printf("---------------------------------------\n");
 }
